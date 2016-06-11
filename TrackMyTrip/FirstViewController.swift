a63f6122d183fd20509f0672180c240fda3dd39c
Mauro Veron <mauroveron@gmail.com>
2016-06-11 13:22:07 +1200

    Initial Commit

---
+++ TrackMyTrip/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  TrackMyTrip
+//
+//  Created by Mauro Verón on 11/06/16.
+//  Copyright © 2016 Mauro Verón. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ TrackMyTrip/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  TrackMyTrip
+//
+//  Created by Mauro Verón on 11/06/16.
+//  Copyright © 2016 Mauro Verón. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
